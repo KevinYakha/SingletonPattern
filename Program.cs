583774169f640ebc63ab0caeb16de883cdb8c1f7
KevinYakha <kevin@yakha.eu>
2024-05-06 15:45:39 +0200

    Created StandardInterestRate Singleton with modifiable interest rate

--- Program.cs
+++ Program.cs
@@ -4,7 +4,17 @@
     {
         public static void Main(string[] args)
         {
+            // Console.WriteLine(StandardInterestRate.interestRate); // an object reference required
+            StandardInterestRate currentRate = StandardInterestRate.getInstance();
+            Console.WriteLine(currentRate.interestRate);
 
+            // changing interest rate
+            currentRate.interestRate = 0.05f;
+
+            // trying to get another instance
+            StandardInterestRate anotherRate = StandardInterestRate.getInstance();
+
+            Console.WriteLine(currentRate.interestRate);
         }
     }
 }
---
+++ StandardInterestRate.cs
@@ -0,0 +1,19 @@
+﻿namespace SingletonPattern
+{
+    public class StandardInterestRate
+    {
+        private static readonly StandardInterestRate instance = new StandardInterestRate();
+
+        public float interestRate = 0.03f;
+
+        private StandardInterestRate()
+        {
+            Console.WriteLine("The Singleton was created");
+        }
+
+        public static StandardInterestRate getInstance()
+        {
+            return instance;
+        }
+    }
+}
